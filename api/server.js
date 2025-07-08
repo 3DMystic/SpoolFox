fb72b7cb02406859cd95494dcc61cf4f11e6674d
Miya Hendren <miyahendren@gmail.com>
2025-07-08 18:39:46 -0400

    added real data to filamentSpools json file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const filamentSpools = require('./filamentSpools');
+const fs = require('fs');
+const filamentSpools = JSON.parse(fs.readFileSync('./filamentSpools.json', 'utf8'));
+
+const path = require('path');
+
+// Serve frontend from public/
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 app.use(express.json());
 
@@ -15,21 +22,22 @@ app.get("/api/filamentSpools", (req, res) => {
     res.json(filamentSpools);
 });
 
+
 // get all filament spools by type
 app.get("/api/filamentSpools/:materialType", (req, res) => {
     const materialType = req.params.materialType.toUpperCase();
     const spools = filamentSpools[materialType];
 
     if (spools) { 
-        res.json(spools);
+        res.status(200).json(spools);
     } else {
-        res.status(404).json({message: `Fliament type @{materialType} not found`});
+        res.status(404).json({message: `Fliament type ${materialType} not found`});
     }
 });
 
 //get single filament spool by id
 app.get("/api/filamentSpools/id/:id", (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id.toLocaleLowerCase();
 
     // merged all filament types into a single array
     const allFilamentTypes = [
@@ -39,16 +47,15 @@ app.get("/api/filamentSpools/id/:id", (req, res) => {
     ];
 
     // find the spool with the matching id
-    const spool = allFilamentTypes.find(spool => spool.id ===id);
-
+    const spool = allFilamentTypes.find(spool => spool.id.toLowerCase() === id);
     if (spool) {
-        res.json(spool);
+        res.status(200).json(spool);
     } else {
         res.status(404).json({message: 'Filament spool not found'});
     }
 });
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Press Ctrl+C to end this process.");
 });
